43f11133c906dbbe3b1bbb733d9cf904f7fd5c5a
Lucas Johnston <lrjohnston@live.nl>
2023-07-31 23:02:31 +0200

    Final delivery

--- [H] Requirements Elicitation/Analysis/mann_whitney/mann_whitney_b.py
+++ [H] Requirements Elicitation/Analysis/mann_whitney/mann_whitney_b.py
@@ -1,135 +1,199 @@
 from scipy import stats
 import csv
+from statistics import mean
 
 def mann_whitney_test(data1, data2):
-    """
-    Performs the Mann-Whitney U test for comparing the means of two variables.
-
-    Parameters:
-        data1 (array-like): The first dataset.
-        data2 (array-like): The second dataset.
-
-    Returns:
-        (float, float): The test statistic and p-value.
-    """
     test_statistic, p_value = stats.mannwhitneyu(data1, data2, alternative='two-sided')
     return test_statistic, p_value
 
-# Example usage
+
+
+## DISTRACTION ##
 data_sets = [
     {
-        's_behavior_more_speed': [1,2,3,3,2,3,3,1,1,1,2,4,1,2,2,1,1,2,1,2,1,1,2,2,2,1],
-        'i_behavior_more_speed': [1,1,2,2,1,2,2,4,1,3,2,3,3,1]
+        's_distraction_manual': [2,2,1,2,1,3,3,1,2,1,1,2,2,4,1,2,2,3,1,2,3,3,1,1,3,2,3,3],
+        'i_distraction_manual': [2,1,2,1,1,2,2,3,3,1,2,2,1,1,2,2]
     },
     {
-        's_behavior_less_speed': [3,1,4,2,1,4,3,3,1,2,2,4,3,2,4,4,1,2,3,3,1,3,4,4,3,3],
-        'i_behavior_less_speed': [2,2,2,2,3,4,3,3,2,3,2,3,3,2]
+        's_distraction_awareness': [4,3,3,4,3,4,3,2,2,1,2,2,3,4,1,3,3,2,2,3,2,2,2,2,3,2,4,2],
+        'i_distraction_awareness': [2,2,2,2,1,2,2,3,2,2,3,1,3,3,2,1]
     },
     {
-        's_behavior_speed_control': [2,2,4,2,1,4,1,2,1,1,2,4,1,2,1,2,1,2,1,2,2,2,4,2,4,1],
-        'i_behavior_speed_control': [2,1,2,3,3,3,3,4,2,3,2,3,3,1]
+        's_distraction_shift_focus': [2,1,2,2,1,3,3,2,2,1,1,1,2,4,1,2,3,3,3,2,2,2,2,1,4,3,2,2],
+        'i_distraction_shift_focus': [2,1,2,1,2,2,3,2,2,1,2,1,2,2,2,2]
     },
     {
-        's_behavior_lane_position': [2,3,3,3,2,4,2,2,2,2,2,4,2,3,3,2,2,3,2,3,2,1,3,4,4,2],
-        'i_behavior_lane_position': [2,2,2,3,3,3,3,3,1,3,3,2,1,2],
+        's_distraction_mental_load': [2,2,2,3,1,3,1,2,2,1,1,1,3,2,1,2,2,1,1,1,1,2,2,2,3,3,2,1],
+        'i_distraction_mental_load': [2,1,2,1,1,2,2,2,1,1,1,1,3,2,2,1]
     },
     {
-        's_behavior_reaction_time': [4,3,2,3,2,3,3,2,1,2,3,4,2,2,3,4,2,3,3,3,2,3,3,3,3,2],
-        'i_behavior_reaction_time': [3,2,2,2,3,3,3,2,1,3,3,2,2,2],
+        's_distraction_glance_frequency': [2,3,2,2,1,3,3,2,2,1,1,2,2,2,2,2,4,2,3,3,4,2,1,1,3,2,3,2],
+        'i_distraction_glance_frequency': [2,1,2,2,1,3,3,2,2,1,2,2,2,3,2,2]
     },
     {
-        's_behavior_wrong_turns': [4,3,4,3,4,2,1,3,1,2,1,4,1,2,4,5,3,4,2,3,1,3,4,4,3,2],
-        'i_behavior_wrong_turns': [2,2,3,3,4,3,2,3,2,1,3,3,3,1],
-    },
-    {
-        's_behavior_operating_errors': [2,1,3,3,2,2,3,1,1,1,2,1,2,2,3,2,1,1,2,2,1,2,3,2,2,2],
-        'i_behavior_operating_errors': [3,2,2,1,1,2,2,3,1,1,2,1,2,1],
+        's_distraction_glance_duration': [3,1,2,2,2,3,4,2,3,2,1,1,2,2,2,2,3,3,2,2,3,3,2,1,2,4,2,3],
+        'i_distraction_glance_duration': [3,1,2,2,2,3,3,2,2,1,2,2,3,3,3,2]
     },
 
 
 
     {
-        'i_behavior_more_speed': [1,1,2,2,1,2,2,4,1,3,2,3,3,1],
-        'z_behavior_more_speed': [2,1,1,2,2,2,1,2,1,1,1],
+        'i_distraction_manual': [2,1,2,1,1,2,2,3,3,1,2,2,1,1,2,2],
+        'z_distraction_manual': [2,1,1,1,3,1,1,2,1,3,4,3,3]
     },
     {
-        'i_behavior_less_speed': [2,2,2,2,3,4,3,3,2,3,2,3,3,2],
-        'z_behavior_less_speed': [4,1,1,1,2,3,1,3,1,5,2],
+        'i_distraction_awareness': [2,2,2,2,1,2,2,3,2,2,3,1,3,3,2,1],
+        'z_distraction_awareness': [2,1,1,2,4,2,1,3,3,5,4,2,2]
     },
     {
-        'i_behavior_speed_control': [2,1,2,3,3,3,3,4,2,3,2,3,3,1],
-        'z_behavior_speed_control': [4,2,1,1,2,2,1,3,2,1,3],
+        'i_distraction_shift_focus': [2,1,2,1,2,2,3,2,2,1,2,1,2,2,2,2],
+        'z_distraction_shift_focus': [2,1,1,1,4,2,2,2,2,3,4,3,2]
     },
     {
-        'i_behavior_lane_position': [2,2,2,3,3,3,3,3,1,3,3,2,1,2],
-        'z_behavior_lane_position': [4,2,1,1,3,2,1,2,2,2,3],
+        'i_distraction_mental_load': [2,1,2,1,1,2,2,2,1,1,1,1,3,2,2,1],
+        'z_distraction_mental_load': [2,1,1,2,4,2,1,2,1,3,1,2,2]
     },
     {
-        'i_behavior_reaction_time': [3,2,2,2,3,3,3,2,1,3,3,2,2,2],
-        'z_behavior_reaction_time': [1,2,1,1,4,3,1,2,3,4,2],
+        'i_distraction_glance_frequency': [2,1,2,2,1,3,3,2,2,1,2,2,2,3,2,2],
+        'z_distraction_glance_frequency': [4,1,1,1,3,1,1,2,3,3,3,3,2]
     },
     {
-        'i_behavior_wrong_turns': [2,2,3,3,4,3,2,3,2,1,3,3,3,1],
-        'z_behavior_wrong_turns': [2,1,1,1,4,1,3,3,2,1,3],
-    },
-    {
-        'i_behavior_operating_errors': [3,2,2,1,1,2,2,3,1,1,2,1,2,1],
-        'z_behavior_operating_errors': [3,1,1,1,3,1,1,2,1,1,2],
+        'i_distraction_glance_duration': [3,1,2,2,2,3,3,2,2,1,2,2,3,3,3,2],
+        'z_distraction_glance_duration': [4,2,1,1,4,2,1,1,1,4,3,2,3]
     },
 
 
 
     {
-        'z_behavior_more_speed': [2,1,1,2,2,2,1,2,1,1,1],
-        's_behavior_more_speed': [1,2,3,3,2,3,3,1,1,1,2,4,1,2,2,1,1,2,1,2,1,1,2,2,2,1],
+        'z_distraction_manual': [2,1,1,1,3,1,1,2,1,3,4,3,3],
+        's_distraction_manual': [2,2,1,2,1,3,3,1,2,1,1,2,2,4,1,2,2,3,1,2,3,3,1,1,3,2,3,3]
     },
     {
-        'z_behavior_less_speed': [4,1,1,1,2,3,1,3,1,5,2],
-        's_behavior_less_speed': [3,1,4,2,1,4,3,3,1,2,2,4,3,2,4,4,1,2,3,3,1,3,4,4,3,3],
+        'z_distraction_awareness': [2,1,1,2,4,2,1,3,3,5,4,2,2],
+        's_distraction_awareness': [4,3,3,4,3,4,3,2,2,1,2,2,3,4,1,3,3,2,2,3,2,2,2,2,3,2,4,2]
     },
     {
-        'z_behavior_speed_control': [4,2,1,1,2,2,1,3,2,1,3],
-        's_behavior_speed_control': [2,2,4,2,1,4,1,2,1,1,2,4,1,2,1,2,1,2,1,2,2,2,4,2,4,1],
+        'z_distraction_shift_focus': [2,1,1,1,4,2,2,2,2,3,4,3,2],
+        's_distraction_shift_focus': [2,1,2,2,1,3,3,2,2,1,1,1,2,4,1,2,3,3,3,2,2,2,2,1,4,3,2,2]
     },
     {
-        'z_behavior_lane_position': [4,2,1,1,3,2,1,2,2,2,3],
-        's_behavior_lane_position': [2,3,3,3,2,4,2,2,2,2,2,4,2,3,3,2,2,3,2,3,2,1,3,4,4,2],
+        'z_distraction_mental_load': [2,1,1,2,4,2,1,2,1,3,1,2,2],
+        's_distraction_mental_load': [2,2,2,3,1,3,1,2,2,1,1,1,3,2,1,2,2,1,1,1,1,2,2,2,3,3,2,1]
     },
     {
-        'z_behavior_reaction_time': [1,2,1,1,4,3,1,2,3,4,2],
-        's_behavior_reaction_time': [4,3,2,3,2,3,3,2,1,2,3,4,2,2,3,4,2,3,3,3,2,3,3,3,3,2],
+        'z_distraction_glance_frequency': [4,1,1,1,3,1,1,2,3,3,3,3,2],
+        's_distraction_glance_frequency': [2,3,2,2,1,3,3,2,2,1,1,2,2,2,2,2,4,2,3,3,4,2,1,1,3,2,3,2]
     },
     {
-        'z_behavior_wrong_turns': [2,1,1,1,4,1,3,3,2,1,3],
-        's_behavior_wrong_turns': [4,3,4,3,4,2,1,3,1,2,1,4,1,2,4,5,3,4,2,3,1,3,4,4,3,2],
-    },
-    {
-        'z_behavior_operating_errors': [3,1,1,1,3,1,1,2,1,1,2],
-        's_behavior_operating_errors': [2,1,3,3,2,2,3,1,1,1,2,1,2,2,3,2,1,1,2,2,1,2,3,2,2,2],
+        'z_distraction_glance_duration': [4,2,1,1,4,2,1,1,1,4,3,2,3],
+        's_distraction_glance_duration': [3,1,2,2,2,3,4,2,3,2,1,1,2,2,2,2,3,3,2,2,3,3,2,1,2,4,2,3]
     },
 ]
 
 
 
-# data1 = []
-# data2 = []
-#
-# test_stat, p_value = mann_whitney_test(data1, data2)
-#
-# # Format the numbers without scientific notation
-# test_stat_formatted = "{:.8f}".format(test_stat)
-# p_value_formatted = "{:.8f}".format(p_value)
-#
-# print("Test statistic:", test_stat_formatted)
-# print("P-value:", p_value_formatted)
 
+
+## BEHAVIOR ##
+# data_sets = [
+#     {
+#         's_behavior_more_speed': [1,2,3,3,2,3,3,1,1,1,2,4,1,2,2,1,1,2,1,2,1,1,2,2,2,1],
+#         'i_behavior_more_speed': [1,1,2,2,1,2,2,4,1,3,2,3,3,1]
+#     },
+#     {
+#         's_behavior_less_speed': [3,1,4,2,1,4,3,3,1,2,2,4,3,2,4,4,1,2,3,3,1,3,4,4,3,3],
+#         'i_behavior_less_speed': [2,2,2,2,3,4,3,3,2,3,2,3,3,2]
+#     },
+#     {
+#         's_behavior_speed_control': [2,2,4,2,1,4,1,2,1,1,2,4,1,2,1,2,1,2,1,2,2,2,4,2,4,1],
+#         'i_behavior_speed_control': [2,1,2,3,3,3,3,4,2,3,2,3,3,1]
+#     },
+#     {
+#         's_behavior_lane_position': [2,3,3,3,2,4,2,2,2,2,2,4,2,3,3,2,2,3,2,3,2,1,3,4,4,2],
+#         'i_behavior_lane_position': [2,2,2,3,3,3,3,3,1,3,3,2,1,2],
+#     },
+#     {
+#         's_behavior_reaction_time': [4,3,2,3,2,3,3,2,1,2,3,4,2,2,3,4,2,3,3,3,2,3,3,3,3,2],
+#         'i_behavior_reaction_time': [3,2,2,2,3,3,3,2,1,3,3,2,2,2],
+#     },
+#     {
+#         's_behavior_wrong_turns': [4,3,4,3,4,2,1,3,1,2,1,4,1,2,4,5,3,4,2,3,1,3,4,4,3,2],
+#         'i_behavior_wrong_turns': [2,2,3,3,4,3,2,3,2,1,3,3,3,1],
+#     },
+#     {
+#         's_behavior_operating_errors': [2,1,3,3,2,2,3,1,1,1,2,1,2,2,3,2,1,1,2,2,1,2,3,2,2,2],
+#         'i_behavior_operating_errors': [3,2,2,1,1,2,2,3,1,1,2,1,2,1],
+#     },
+
+
+
+    # {
+    #     'i_behavior_more_speed': [1,1,2,2,1,2,2,4,1,3,2,3,3,1],
+    #     'z_behavior_more_speed': [2,1,1,2,2,2,1,2,1,1,1],
+    # },
+    # {
+    #     'i_behavior_less_speed': [2,2,2,2,3,4,3,3,2,3,2,3,3,2],
+    #     'z_behavior_less_speed': [4,1,1,1,2,3,1,3,1,5,2],
+    # },
+    # {
+    #     'i_behavior_speed_control': [2,1,2,3,3,3,3,4,2,3,2,3,3,1],
+    #     'z_behavior_speed_control': [4,2,1,1,2,2,1,3,2,1,3],
+    # },
+    # {
+    #     'i_behavior_lane_position': [2,2,2,3,3,3,3,3,1,3,3,2,1,2],
+    #     'z_behavior_lane_position': [4,2,1,1,3,2,1,2,2,2,3],
+    # },
+    # {
+    #     'i_behavior_reaction_time': [3,2,2,2,3,3,3,2,1,3,3,2,2,2],
+    #     'z_behavior_reaction_time': [1,2,1,1,4,3,1,2,3,4,2],
+    # },
+    # {
+    #     'i_behavior_wrong_turns': [2,2,3,3,4,3,2,3,2,1,3,3,3,1],
+    #     'z_behavior_wrong_turns': [2,1,1,1,4,1,3,3,2,1,3],
+    # },
+    # {
+    #     'i_behavior_operating_errors': [3,2,2,1,1,2,2,3,1,1,2,1,2,1],
+    #     'z_behavior_operating_errors': [3,1,1,1,3,1,1,2,1,1,2],
+    # },
+
+
+
+#     {
+#         'z_behavior_more_speed': [2,1,1,2,2,2,1,2,1,1,1],
+#         's_behavior_more_speed': [1,2,3,3,2,3,3,1,1,1,2,4,1,2,2,1,1,2,1,2,1,1,2,2,2,1],
+#     },
+#     {
+#         'z_behavior_less_speed': [4,1,1,1,2,3,1,3,1,5,2],
+#         's_behavior_less_speed': [3,1,4,2,1,4,3,3,1,2,2,4,3,2,4,4,1,2,3,3,1,3,4,4,3,3],
+#     },
+#     {
+#         'z_behavior_speed_control': [4,2,1,1,2,2,1,3,2,1,3],
+#         's_behavior_speed_control': [2,2,4,2,1,4,1,2,1,1,2,4,1,2,1,2,1,2,1,2,2,2,4,2,4,1],
+#     },
+#     {
+#         'z_behavior_lane_position': [4,2,1,1,3,2,1,2,2,2,3],
+#         's_behavior_lane_position': [2,3,3,3,2,4,2,2,2,2,2,4,2,3,3,2,2,3,2,3,2,1,3,4,4,2],
+#     },
+#     {
+#         'z_behavior_reaction_time': [1,2,1,1,4,3,1,2,3,4,2],
+#         's_behavior_reaction_time': [4,3,2,3,2,3,3,2,1,2,3,4,2,2,3,4,2,3,3,3,2,3,3,3,3,2],
+#     },
+#     {
+#         'z_behavior_wrong_turns': [2,1,1,1,4,1,3,3,2,1,3],
+#         's_behavior_wrong_turns': [4,3,4,3,4,2,1,3,1,2,1,4,1,2,4,5,3,4,2,3,1,3,4,4,3,2],
+#     },
+#     {
+#         'z_behavior_operating_errors': [3,1,1,1,3,1,1,2,1,1,2],
+#         's_behavior_operating_errors': [2,1,3,3,2,2,3,1,1,1,2,1,2,2,3,2,1,1,2,2,1,2,3,2,2,2],
+#     },
+# ]
 
 # Open a file in write mode and create a CSV writer
 with open('output.csv', 'w', newline='') as file:
     writer = csv.writer(file)
 
     # Write the header row
-    writer.writerow(['test', 'p'])
+    writer.writerow(['test', 'n_1', 'n_2', 'diff',  'U', 'r', 'p'])
 
     # Loop over the sets
     for data_set in data_sets:
@@ -144,16 +208,23 @@ with open('output.csv', 'w', newline='') as file:
             # Do M-W tests
             test_stat, p_value = mann_whitney_test(subset1, subset2)
 
+            n_1 = len(subset1)
+            n_2 = len(subset2)
+            effect_size = abs(1 - ((2 * test_stat) / (n_1 * n_2))) # Use absolute value cause we're not so interested yet in the direction
+            mean_diff = abs(mean(subset1) - mean(subset2)) # Use absolute value cause we're not so interested yet in the direction
+
             # Format the numbers without scientific notation
             test_stat_formatted = "{:.8f}".format(test_stat)
+            r_value_formatted = "{:.8f}".format(effect_size)
             p_value_formatted = "{:.8f}".format(p_value)
-
-
+            mean_diff_formatted = "{:.8f}".format(mean_diff)
 
             # Write the row to the CSV file
             keys = list(data_set.keys())
             test_name = keys[0] + " vs. " + keys[1]
-            writer.writerow([test_name, p_value_formatted])
-            # print(keys[0], "vs.", keys[1])
-            # print("Test statistic:", test_stat_formatted)
-            # print("P-value:", p_value_formatted)
+            writer.writerow([test_name, n_1, n_2, mean_diff_formatted, test_stat_formatted, r_value_formatted, p_value_formatted])
+            print("\n\n", keys[0], "vs.", keys[1])
+            print("Test statistic:", test_stat_formatted)
+            print("r-value:", r_value_formatted)
+            print("p-value:", p_value_formatted)
+            print("Mean diff:", mean_diff_formatted)
--- [H] Requirements Elicitation/Analysis/mann_whitney/mann_whitney_c.py
+++ [H] Requirements Elicitation/Analysis/mann_whitney/mann_whitney_c.py
@@ -1,31 +1,76 @@
 from scipy import stats
 
 def mann_whitney_test(data1, data2):
-    """
-    Performs the Mann-Whitney U test for comparing the means of two variables.
-
-    Parameters:
-        data1 (array-like): The first dataset.
-        data2 (array-like): The second dataset.
-
-    Returns:
-        (float, float): The test statistic and p-value.
-    """
     test_statistic, p_value = stats.mannwhitneyu(data1, data2, alternative='two-sided')
     return test_statistic, p_value
 
-
-
-data1 = [1,2,1,1,3,2,2,2,1,1,2
-]
-data2 = [4,2,1,2,1,2,2,1,3,3,2,3,2,3,3,1,2,1,1,2,1,3,3,2,2
-]
-
+data1 = [1,2,1,1,3,2,2,2,1,1,2]
+data2 = [4,2,1,2,1,2,2,1,3,3,2,3,2,3,3,1,2,1,1,2,1,3,3,2,2]
 test_stat, p_value = mann_whitney_test(data1, data2)
+r_value = test_stat / (len(data1) * len(data2))
+print("\ndata1:")
+print(stats.describe(data1))
+print("\ndata2:")
+print(stats.describe(data2))
+
+# https://doi.org/10.1177/1536867X1201200202 Ronan M. Conroy
+# conroy_set_0 = [5, 5, 5, 5, 5, 5, 7, 8, 9, 10]
+# conroy_set_1 = [1, 2, 3, 4, 5, 5, 5, 5, 5, 5]
+# test_stat, p_value = mann_whitney_test(conroy_set_0, conroy_set_1)
+# r_value = test_stat / (len(conroy_set_0) * len(conroy_set_1))
+# print("\nConroy Set 0:")
+# print(stats.describe(conroy_set_0))
+# print("\nConroy Set 1:")
+# print(stats.describe(conroy_set_1))
+
+# Means are very different
+# try_set_a = [4, 3, 2, 3, 4, 2, 5, 3, 4, 2, 3, 4, 5, 3, 4, 2, 4, 5, 3, 4]
+# try_set_b = [1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 2, 1]
+# U-value: 386.00000000
+# p-value: 0.00000022
+# r-value: 0.96500000
+
+# Means are somewhat different
+# try_set_a = [3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5]
+# try_set_b = [1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5]
+# U-value: 312.00000000
+# p-value: 0.00178101
+# r-value: 0.78000000
+
+# Means are slightly different
+# try_set_a = [3, 3, 3, 3, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+# try_set_b = [5, 3, 2, 2, 2, 4, 3, 3, 5, 6, 4, 3, 3, 3, 3, 4, 3, 3, 3, 3]
+# U-value: 268.00000000
+# p-value: 0.04837838
+# r-value: 0.67000000
+
+# Means for which the difference is not statistically significant
+# try_set_a = [3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+# try_set_b = [2, 2, 2, 4, 2, 3, 5, 3, 3, 5, 3, 3, 6, 5, 3, 4, 4, 3, 3, 3]
+# U-value: 260.00000000
+# p-value: 0.08355513
+# r-value: 0.65000000
+
+# Means are fairly similar
+# try_set_a = [3, 3, 3, 3, 2, 2, 3, 3, 3, 3, 3, 3, 2, 2, 3, 3, 3, 3, 3, 3]
+# try_set_b = [2.8, 2.9, 2.75, 2.8, 3.2, 3.1, 2.5, 2.1, 2.99, 3.33, 3.01, 2.98, 2.85, 3.04, 2.6, 2.7, 2.8, 2.9, 2.95, 2.91]
+# U-value: 240.00000000
+# p-value: 0.26911167
+# r-value: 0.60000000
+
+# test_stat, p_value = mann_whitney_test(try_set_a, try_set_b)
+# r_value = test_stat / (len(try_set_a) * len(try_set_b))
+# print("\nTry Set a:")
+# print(stats.describe(try_set_a))
+# print("\nTry Set b:")
+# print(stats.describe(try_set_b))
 
 # Format the numbers without scientific notation
 test_stat_formatted = "{:.8f}".format(test_stat)
 p_value_formatted = "{:.8f}".format(p_value)
+r_value_formatted = "{:.8f}".format(r_value)
 
-print("Test statistic:", test_stat_formatted)
-print("P-value:", p_value_formatted)
+print("\nResults Mann-Whitney:")
+print("U-value:", test_stat_formatted)
+print("p-value:", p_value_formatted)
+print("r-value:", r_value_formatted)
